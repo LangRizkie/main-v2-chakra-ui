b0c3a5e6661899f2c6e22007f2b4ac79d17f15ee
Gilang Rizkie <langrizkie@gmail.com>
2025-03-25 00:15:22 +0700

    feat: add network status

--- src/app/context.tsx
+++ src/app/context.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import { useDisclosure, UseDisclosureReturn } from '@chakra-ui/react'
-import React, { createContext } from 'react'
+import { useNetwork } from 'ahooks'
+import { createContext, useEffect } from 'react'
 import { Layout } from '@/types/default'
+import toast from '@/utilities/toast'
 
 export const ModalReference: UseDisclosureReturn = {
 	onClose: () => {},
@@ -14,6 +18,16 @@ export const ModalContext = createContext<UseDisclosureReturn>(ModalReference)
 
 const Context: React.FC<Layout> = ({ children }) => {
 	const modal = useDisclosure()
+	const network = useNetwork()
+
+	useEffect(() => {
+		if (!network.online) {
+			toast.error({
+				description: 'You seems to be offline, please check your internet connection.',
+				title: 'Disconnected'
+			})
+		}
+	}, [network.online])
 
 	Object.assign(ModalReference, modal)
 
